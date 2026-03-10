054f9febe557b28452a34ecc0f0a4831ac39f656
Tim Dorr <git@timdorr.com>
2026-03-10 10:28:29 -0400

    Add social share stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://timdorrforgeorgia.com',
   output: 'server',
   adapter: cloudflare({ imageService: 'cloudflare' }),
 
